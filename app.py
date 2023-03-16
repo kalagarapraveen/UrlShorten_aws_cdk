f1c7e73c2f13d7a5b911aedc6a75f36562e2581e
Praveen Kumar Kalagara <praveen.kalagara@moreretail.in>
2023-03-16 10:28:50 +0530

    added os variables in pipeline

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ from cdkstack.UrlLayerstack import UrlLayersStack
 app = cdk.App()
 
 UrlLayersStack(app, "UrlLayersStack", env=cdk.Environment(
-    account=os.getenv['CDK_DEFAULT_ACCOUNT_PERSONAL'],
+    account=os.environ['CDK_DEFAULT_ACCOUNT_PERSONAL'],
     region=os.environ['CDK_DEFAULT_REGION']
     ), description="URL Layers stack that creates Lambda layers for URL shorten project"
 )
 
 
 URLstack(app, "URLstack", env=cdk.Environment(
-    account=os.getenv['CDK_DEFAULT_ACCOUNT_PERSONAL'],
+    account=os.environ['CDK_DEFAULT_ACCOUNT_PERSONAL'],
     region=os.environ['CDK_DEFAULT_REGION']
     ), description="URL Layers stack that creates Lambda layer for URL shorten project"
 )
